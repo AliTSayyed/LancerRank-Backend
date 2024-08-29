b6d38c22c021b09a84eb45e1586fa6c5a75a498a
alitsayyed <alitsayyed@gmail.com>
2024-08-29 18:13:53 -0500

    updated packages

--- src/trust_pilot_backend/wsgi.py
+++ src/trust_pilot_backend/wsgi.py
@@ -9,10 +9,9 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 
 import os
 
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trust_pilot_backend.settings')
 
 application = get_wsgi_application()
-
-app = application
